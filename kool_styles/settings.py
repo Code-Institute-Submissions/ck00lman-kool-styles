766f98f0fb61a39d67c1818ce5fe8a6d767bcbb9
ck00lman <ckoolman1985@gmail.com>
2020-09-29 23:37:30 +0000

    Installed gunicorn and added it to requirements file, disabled the collection of static file before deploying to heroku and added Procfile

--- kool_styles/settings.py
+++ kool_styles/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'n4$x(5r_2=t7n5^5!g#9icz-t2&w$+hwmxu8)^r++hsakh18or'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kool-styles.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'kool_styles.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
