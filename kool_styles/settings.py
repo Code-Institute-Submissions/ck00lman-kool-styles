7fc82a792c79c50ea69429164cd25cb6b001a306
ck00lman <ckoolman1985@gmail.com>
2020-09-30 18:34:44 +0000

    Removed Google Login Django

--- kool_styles/settings.py
+++ kool_styles/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kool-styles.herokuapp.com', 'localhost']
 
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
     'home',
     'products',
     'bag',
