0517ec824268002b045c8146284a40b380885f1d
ck00lman <ckoolman1985@gmail.com>
2020-09-30 03:39:01 +0000

    Added cache control

--- kool_styles/settings.py
+++ kool_styles/settings.py
@@ -167,6 +167,12 @@ USE_TZ = True
 
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'kool-styles'
     AWS_S3_REGION_NAME = 'us-east-2'
