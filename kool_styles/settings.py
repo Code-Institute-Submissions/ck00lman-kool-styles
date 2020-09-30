f755dfe655fefc6c176841c89a80e959dd4180ad
ck00lman <ckoolman1985@gmail.com>
2020-09-30 00:07:31 +0000

    Secured app by removing Secret Key and setting a new one in  environment variable and setting Debug only if environment if in development

--- kool_styles/settings.py
+++ kool_styles/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n4$x(5r_2=t7n5^5!g#9icz-t2&w$+hwmxu8)^r++hsakh18or'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kool-styles.herokuapp.com', 'localhost']
 
