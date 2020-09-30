909d1f02c23bbfec133d44f051750ae11f970ae2
ck00lman <ckoolman1985@gmail.com>
2020-09-30 15:51:26 +0000

    added allowed hosts as instructed and  google to installed apps

--- kool_styles/settings.py
+++ kool_styles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kool-styles.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'home',
     'products',
     'bag',
