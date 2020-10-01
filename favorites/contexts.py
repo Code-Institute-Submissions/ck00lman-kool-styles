746062f9ae5967df17409477e288f7ee323ceef7
ck00lman <ckoolman1985@gmail.com>
2020-10-01 03:02:35 +0000

    Updated favorites.html and context.py with favorites_total

--- favorites/contexts.py
+++ favorites/contexts.py
@@ -6,14 +6,14 @@ from products.models import Product
 def favorites_contents(request):
 
     favorites_items = []
-    total = 0
+    favorites_total = 0
     product_count = 0
     favorites = request.session.get('favorites', {})
 
     for item_id, item_data in favorites.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
+            favorites_total += item_data * product.price
             product_count += item_data
             favorites_items.append({
                 'item_id': item_id,
@@ -23,7 +23,7 @@ def favorites_contents(request):
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
-                total += quantity * product.price
+                favorites_total += quantity * product.price
                 product_count += quantity
                 favorites_items.append({
                     'item_id': item_id,
@@ -32,9 +32,9 @@ def favorites_contents(request):
                     'size': size,
                 })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
-        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    if favorites_total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = favorites_total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - favorites_total
     else:
         delivery = 0
         free_delivery_delta = 0
@@ -43,7 +43,7 @@ def favorites_contents(request):
     
     context = {
         'favorites_items': favorites_items,
-        'total': total,
+        'favorites_total': favorites_total,
         'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
